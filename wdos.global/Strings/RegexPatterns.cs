a077a91c673b3f38566d0740f5eff313ed87ddc2
Dynesshely <2369913161@qq.com>
2022-03-12 17:59:01 +0800

    📦 feat: Global info and methods library

---
+++ wdos.global/Strings/RegexPatterns.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wdos.global.Strings
+{
+    public static class RegexPatterns
+    {
+        public const string GUID_Pattern = "([A-Z]*[0-9]*)*(-)*";
+    }
+}
