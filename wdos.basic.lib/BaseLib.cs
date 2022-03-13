4d1eec0b2f315406ef70eaf366da45c6954881c1
Dynesshely <2369913161@qq.com>
2022-03-13 20:54:35 +0800

    📦 clean

---
+++ wdos.basic.lib/BaseLib.cs
@@ -0,0 +1,19 @@
+﻿namespace wdos.basic.lib
+{
+    public class BaseLib
+    {
+
+        /// <summary>
+        /// 生成指定数量的字符
+        /// </summary>
+        /// <param name="num">数量</param>
+        /// <param name="c">字符</param>
+        /// <returns>拼合成字符串</returns>
+        private static string GenerateChar(int num, char c)
+        {
+            string space = "";
+            for (int i = 0; i < num; i++) space += c;
+            return space;
+        }
+    }
+}
--- wdos.contract/IContract.cs
+++ wdos.contract/IContract.cs
@@ -32,19 +32,6 @@ namespace wdos.contract
     /// </summary>
     public static class GUID_Helper
     {
-        /// <summary>
-        /// 生成指定数量的字符
-        /// </summary>
-        /// <param name="num">数量</param>
-        /// <param name="c">字符</param>
-        /// <returns>拼合成字符串</returns>
-        private static string GenerateChar(int num, char c)
-        {
-            string space = "";
-            for (int i = 0; i < num; i++) space += c;
-            return space;
-        }
-
         public static GUID_Status TypeOf_GUID_Staus(App_GUID_Struct guid)
         {
             GUID_Status status = new();
