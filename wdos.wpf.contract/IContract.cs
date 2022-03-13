a016d903fce777de1d2ca67631c06c77abfcd9f2
Dynesshely <2369913161@qq.com>
2022-03-13 11:36:25 +0800

    📝 chore: nothing...

--- wdos.wpf.contract/IContract.cs
+++ wdos.wpf.contract/IContract.cs
@@ -1,4 +1,4 @@
-using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using BaseContract = wdos.contract.IContract;
 
@@ -10,6 +10,42 @@ namespace wdos.wpf.contract
     [Description("WPF-Plugin-Interface")]
     public interface IContract : BaseContract
     {
+        /// <summary>
+        /// 要求应用检查运行环境
+        /// </summary>
+        /// <returns>能否运行以及情况</returns>
+        EnvironmentStatus CheckEnvironment();
 
+        /// <summary>
+        /// 启动应用
+        /// </summary>
+        /// <returns>是否启动成功</returns>
+        bool Start();        
+    }
+
+    /// <summary>
+    /// 环境情况结构体
+    /// </summary>
+    public struct EnvironmentStatus
+    {
+        public List<DependencyStatus> dependencies;
+        public bool readyToRun;
+    }
+
+    /// <summary>
+    /// 依赖项状况结构体
+    /// </summary>
+    public struct DependencyStatus
+    {
+        public string name, message;
+        public Dictionary<string, bool> versions;
+    }
+
+    /// <summary>
+    /// 状况级别
+    /// </summary>
+    public enum StatusLevel
+    {
+        Info = 0, Warning = 1, Error = 2, Fatal = 3
     }
 }
